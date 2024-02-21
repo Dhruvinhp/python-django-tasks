bb8a7fdebe8b5d7991f101bc4899d2c1183d9f88
Dhruvin Prajapati <dhruvinhprajapati@gmail.com>
2024-02-21 16:29:14 +0530

    fix: task 1 defination

--- task1/main.py
+++ task1/main.py
@@ -1,8 +1,3 @@
-# Task: Given a number (positive, negative, or 0), return the logical negation (as a 1 or 0) of that
-# number. Do so using only bitwise operators. Any of these characters/constructs are not allowed
-# if, do, while, for, -, not, or, and, is, [,] and any functions.
-
-
 def logical_negation(num):
     return (num ^ 1) & 1
 
